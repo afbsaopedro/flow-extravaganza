985b65666f9422f3d5dcf84815e8f521b3793326
André São Pedro <80008703+afbsaopedro@users.noreply.github.com>
2024-07-11 13:33:39 +0100

    Readded app.vue

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup>
+const token = useState('token')
+token.value = 'no.token.set'
+</script>
+
+<template>
+    <div>
+        {{ token }}
+      <NuxtLayout>
+        <NuxtPage/>
+      </NuxtLayout>
+    </div>
+  </template>
