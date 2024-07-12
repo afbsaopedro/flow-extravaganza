ac2bb0f3589570f257444f01e0c74cbcc38a4738
André São Pedro <80008703+afbsaopedro@users.noreply.github.com>
2024-07-12 13:58:58 +0100

    Removed divider in modal dialogue

--- pages/signup.vue
+++ pages/signup.vue
@@ -40,13 +40,13 @@ async function postEmail() {
     <UButton class="my-3" label="Submit" @click="postEmail"></UButton>
 
     <UModal v-model="isOpen" prevent-close>
-      <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+      <UCard :ui="{ ring: '', divide: '' }">
         <template #header>
           <div class="flex items-center justify-between">
             <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
               Redirect
             </h3>
-            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = false" />
+            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="my-1" @click="isOpen = false" />
           </div>
         </template>
         <p>You will be redirect to the log in page.</p>
