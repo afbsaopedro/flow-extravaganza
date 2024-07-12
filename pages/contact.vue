53ebeb59154677e24e5c900e194b97dd026ec3e0
André São Pedro <80008703+afbsaopedro@users.noreply.github.com>
2024-07-12 13:39:06 +0100

    Added contact page

---
+++ pages/contact.vue
@@ -0,0 +1,55 @@
+<script setup>
+const contactEmails = [
+    {
+        email: 'first.contact.email@service.com',
+        holder: 'Name Surname'
+    },
+    {
+        email: 'second.contact.email@service.com',
+        holder: 'Name Surname'
+    },
+]
+
+const contactPhoneNumbers = [
+    {
+        number: '+351 696 969 696',
+        holder: 'Sr. Manel'
+    },
+    {
+        number: '6+351 696 969 696',
+        holder: 'Sra. Joaquina'
+    },
+]
+
+const items = [{
+    label: 'E-mail',
+    icon: 'i-heroicons-envelope',
+    slot: 'email'
+    },
+    {
+        label: 'Phone',
+    icon: 'i-heroicons-phone',
+    slot: 'phone'
+    }
+]
+</script>
+
+<template>
+    <h1>Contacts</h1>
+    <UAccordion :items="items">
+    <template #email>
+      <div class="text-gray-900 dark:text-white text-center">
+        <ul class="list-disc list-inside text-left">
+            <li v-for="email of contactEmails">{{ email.email }} - {{ email.holder }}</li>
+        </ul>
+      </div>
+    </template>
+    <template #phone>
+      <div class="text-gray-900 dark:text-white text-center">
+        <ul class="list-disc list-inside text-left">
+            <li v-for="phone of contactPhoneNumbers">{{ phone.number }} - {{ phone.holder }}</li>
+        </ul>
+      </div>
+    </template>
+  </UAccordion>
+</template>
