b2112a29f9407935fea6a3137232a7a3e5d7796b
André São Pedro <80008703+afbsaopedro@users.noreply.github.com>
2024-07-14 18:12:52 +0100

    Added config file

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+import { _primary } from "#tailwind-config/theme/accentColor";
+
+export default defineAppConfig({
+    ui: {
+      notifications: {
+        // Show toasts at the top right of the screen
+        position: 'top-0 bottom-auto'
+      }
+    },
+  })
+  
